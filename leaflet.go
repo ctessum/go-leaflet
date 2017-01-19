c29ec9c3072b904e2fa16c9ae67fbce2993ab47d
Chris Tessum <ctessum@gmail.com>
2017-01-19 14:43:57 -0800

    Add options

--- leaflet.go
+++ leaflet.go
@@ -13,15 +13,46 @@ type Map struct {
 }
 
 // NewMap creates a new map in the div specified by divID.
-func NewMap(divID string) *Map {
+func NewMap(divID string, options *MapOptions) *Map {
 	return &Map{
 		Object: l.Call("map", divID),
 	}
 }
 
+// DefaultMapOptions returns the default Map options.
+func DefaultMapOptions() *MapOptions {
+	return &MapOptions{
+		Object: js.Global.Get("Object").New(),
+	}
+}
+
+// MapOptions specify the options for a map:
+// http://leafletjs.com/reference-1.0.2.html#map.
+// They need to be initialized with DefaultMapOptions.
+type MapOptions struct {
+	Object       *js.Object
+	PreferCanvas bool `js:"preferCanvas"`
+}
+
 // SetView sets the center and zoom level of the map.
 func (m *Map) SetView(center *LatLng, zoom int) {
 	m.Object.Call("setView", center, zoom)
+}
+
+// CreatePane creates a new Pane with the given name:
+// http://leafletjs.com/reference-1.0.2.html#map-createpane
+func (m *Map) CreatePane(name string) *Pane {
+	return &Pane{Object: m.Object.Call("createPane", name)}
+}
+
+// Pane is a leaflet pane.
+type Pane struct {
+	*js.Object
+}
+
+// SetZIndex sets the Z index of the pane.
+func (p *Pane) SetZIndex(index int) {
+	p.Object.Get("style").Set("zIndex", index)
 }
 
 // LatLng specifies a point in latitude and longitude
@@ -52,6 +83,7 @@ func NewTileLayer(urlTemplate string, options *TileLayerOptions) *TileLayer {
 }
 
 // TileLayerOptions specifies the options for the TileLayer: http://leafletjs.com/reference-1.0.2.html#tilelayer-option
+// They need to be initialized with DefaultTileLayerOptions.
 type TileLayerOptions struct {
 	*js.Object
 	MinZoom       int      `js:"minZoom"`
@@ -90,6 +122,38 @@ func (l *Layer) AddTo(m *Map) {
 // Path is a leaflet path object: http://leafletjs.com/reference-1.0.2.html#path.
 type Path struct {
 	Layer
+}
+
+// SetStyle sets the style of the receiver:
+// http://leafletjs.com/reference-1.0.2.html#path-setstyle.
+func (p *Path) SetStyle(style *PathOptions) {
+	p.Object.Call("setStyle", style)
+}
+
+// PathOptions specify the options for a path:
+// http://leafletjs.com/reference-1.0.2.html#path-option.
+// They need to be initialized with DefaultPathOptions.
+type PathOptions struct {
+	Object      *js.Object
+	Stroke      bool    `js:"stroke"`
+	Color       string  `js:"color"`
+	Weight      int     `js:"weight"`
+	Opacity     float64 `js:"opacity"`
+	LineCap     string  `js:"lineCap"`
+	LineJoin    string  `js:"lineJoin"`
+	DashArray   string  `js:"dashArray"`
+	DashOffset  string  `js:"dashOffset"`
+	Fill        bool    `js:"fill"`
+	FillColor   string  `js:"fillColor"`
+	FillOpacity float64 `js:"fillOpacity"`
+	FillRule    string  `js:"fillRule"`
+}
+
+// DefaultPathOptions returns the default TileLayer options.
+func DefaultPathOptions() *PathOptions {
+	return &PathOptions{
+		Object: js.Global.Get("Object").New(),
+	}
 }
 
 // Polyline is a leaflet polyline object: http://leafletjs.com/reference-1.0.2.html#polyline.
